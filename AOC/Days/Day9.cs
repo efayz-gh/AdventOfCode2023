39ed500680c57f3feb25e6bdfb3a1dd876ced726
efayz <39465488+efayz-gh@users.noreply.github.com>
2023-12-11 13:10:07 +0100

    day 9

---
+++ AOC/Days/Day9.cs
@@ -0,0 +1,30 @@
+﻿namespace AOC.Days;
+
+public class Day9 : Day
+{
+    public override string Part1() =>
+        File.ReadAllLines(FileInput)
+            .Select(line => line
+                .Split(" ")
+                .Select(long.Parse)
+                .ToList())
+            .Select(Predict)
+            .Sum()
+            .ToString();
+
+    public override string Part2() =>
+        File.ReadAllLines(FileInput)
+            .Select(line => line
+                .Split(" ")
+                .Select(long.Parse)
+                .Reverse()
+                .ToList())
+            .Select(Predict)
+            .Sum()
+            .ToString();
+
+    private static long Predict(List<long> sequence) =>
+        sequence.All(x => x == 0)
+            ? 0
+            : sequence[^1] + Predict(sequence.Zip(sequence.Skip(1), (a, b) => b - a).ToList());
+}
